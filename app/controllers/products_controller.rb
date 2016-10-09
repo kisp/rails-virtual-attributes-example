18aff76d6d406b3824611d5fee9bc7f1a55c63b4
Kilian Sprotte <kilian.sprotte@gmail.com>
2016-10-09 05:52:24 +0000

    released at text

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :price_in_cents, :released_at)
+      params.require(:product).permit(:name, :price_in_dollars, :released_at_text)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,38 @@
 class Product < ApplicationRecord
+    
+    attr_writer :released_at_text
+    
+
+    
+    attr_writer :released_at_text
+    before_save :save_released_at_text  
+    validate :check_released_at_text
+  
+
+    
+    def price_in_dollars
+        price_in_cents.to_d/100 if price_in_cents
+    end
+
+    def price_in_dollars=(dollars)
+        self.price_in_cents = dollars.to_d * 100 if dollars.present?
+    end
+    
+
+    def released_at_text
+        @released_at_text || released_at.try(:strftime, "%Y-%m-%d %H:%M:%S")
+    end
+  
+  def save_released_at_text
+    self.released_at = Time.zone.parse(@released_at_text) if @released_at_text.present?
+  end
+  
+  def check_released_at_text
+    if  @released_at_text.present? && Time.zone.parse(@released_at_text).nil?
+      errors.add :released_at_text, "cannot be parsed"
+    end
+  rescue ArgumentError
+    errors.add :released_at_text, "is out of range"   
+  end
+  
 end
